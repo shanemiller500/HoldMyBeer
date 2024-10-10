c2cd6fde1602cdfa7bb50b8f7fef62a1cb856bcf
shanemiller500 <shanemiller500@gmail.com>
2024-10-10 14:12:36 -0600

    link update

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -13,7 +13,7 @@ export default function FeaturesBlocks() {
           <div className="max-w-xs mx-auto sm:max-w-none md:max-w-xl lg:max-w-none grid sm:grid-cols-4 gap-4 lg:gap-6 lg:grid-cols-4" data-aos-id-featbl>
 
             {/* 1st item */}
-            <a className="flex flex-col p-5 group text-white bg-gradient-to-tr from-teal-500 to-teal-400 dark:to-teal-500 shadow-2xl" href="https://u-mail.ai" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]">
+            <a className="flex flex-col p-5 group text-white bg-gradient-to-tr from-teal-500 to-teal-400 dark:to-teal-500 shadow-2xl" href="https://u-mail.ai" target="_blank" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]">
               <svg className="w-8 h-8 mb-3" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <path className="fill-current" d="M19 18.414l-4 4L9.586 17l.707-.707L12 14.586V8.414l-5-5L4.414 6l6.293 6.293-1.414 1.414L1.586 6 7 .586l7 7v5l8.463-8.463a3.828 3.828 0 115.414 5.414L21 16.414v6.172l5 5L28.586 25l-6.293-6.293 1.414-1.414L31.414 25 26 30.414l-7-7v-5zm-4 1.172L26.463 8.123a1.828 1.828 0 10-2.586-2.586L12.414 17 15 19.586zM11 30v2C4.925 32 0 27.075 0 21h2a9 9 0 009 9zm0-5v2a6 6 0 01-6-6h2a4 4 0 004 4z" fillRule="nonzero" />
               </svg>
@@ -25,7 +25,7 @@ export default function FeaturesBlocks() {
             </a>
 
             {/* 2nd item */}
-            <a className="flex flex-col p-5 group text-white bg-gradient-to-tr from-purple-500 to-purple-400 dark:to-purple-500 shadow-2xl" href="https://applypro.ai" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]" data-aos-delay="100">
+            <a className="flex flex-col p-5 group text-white bg-gradient-to-tr from-purple-500 to-purple-400 dark:to-purple-500 shadow-2xl" href="https://applypro.ai" target="_blank" data-aos="fade-down" data-aos-anchor="[data-aos-id-featbl]" data-aos-delay="100">
               <svg className="w-8 h-8 mb-3" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <path className="fill-current" d="M20.796 20l-1.677 7.264a6 6 0 01-7.302 4.471L0 28.781V11.54l.35-.3 9.457-8.107a3.751 3.751 0 015.29 5.29L11.175 13H28.5a3.5 3.5 0 012.6 1.156c.663.736.984 1.72.878 2.74-.252 1.808-1.817 3.143-3.622 3.104h-7.56zM2 27.22l10.303 2.575a4 4 0 004.868-2.98L19.204 18h9.173c.812.018 1.508-.575 1.615-1.345A1.5 1.5 0 0028.5 15H11.173a2 2 0 01-1.517-3.3l3.922-4.577a1.755 1.755 0 00-.597-2.733 1.751 1.751 0 00-1.872.262L2 12.46v14.76zM28 .585L29.414 2 23 8.414 21.586 7 28 .586zm-8.272 6.627l-1.94-.485 1.484-5.94 1.94.484-1.484 5.94zm3.544 5l-.485-1.94 5.94-1.486.486 1.94-5.94 1.486z" fillRule="nonzero" />
               </svg>
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -91,7 +91,7 @@ export default function Footer() {
                     </a>
                   </li>
                   <li className="mb-1">
-                    <a className="text-gray-600 dark:text-gray-400 hover:underline transition duration-150 ease-in-out" href="https://www.amazon.com/JYVNCZ-Reversible-Pillowcase-Decorative-Inches%EF%BC%88Gold%EF%BC%89/dp/B0BG8HW1DV/ref=asc_df_B0BG8HW1DV/?tag=hyprod-20&linkCode=df0&hvadid=693769995797&hvpos=&hvnetw=g&hvrand=12613000348124047769&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9028890&hvtargid=pla-2068535422238&mcid=542f2c86667831afae14b3cc9ec173fe&th=1">
+                    <a className="text-gray-600 dark:text-gray-400 hover:underline transition duration-150 ease-in-out" href="https://www.amazon.com/JYVNCZ-Reversible-Pillowcase-Decorative-Inches%EF%BC%88Gold%EF%BC%89/dp/B0BG8HW1DV/ref=asc_df_B0BG8HW1DV/?tag=hyprod-20&linkCode=df0&hvadid=693769995797&hvpos=&hvnetw=g&hvrand=12613000348124047769&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9028890&hvtargid=pla-2068535422238&mcid=542f2c86667831afae14b3cc9ec173fe&th=1" target="_blank">
                     Nicolas Cage Sequin Pillow                    </a>
                   </li>
                   {/* <li className="mb-1">
@@ -113,12 +113,12 @@ export default function Footer() {
                 <h6 className="font-medium uppercase mb-2">Products</h6>
                 <ul>
                   <li className="mb-1">
-                    <a className="text-gray-600 dark:text-gray-400 hover:underline transition duration-150 ease-in-out" href="https://applypro.ai">
+                    <a className="text-gray-600 dark:text-gray-400 hover:underline transition duration-150 ease-in-out" href="https://applypro.ai" target="_blank">
                      ApplyPro.ai
                     </a>
                   </li>
                   <li className="mb-1">
-                    <a className="text-gray-600 dark:text-gray-400 hover:underline transition duration-150 ease-in-out" href="https://u-mail.ai">
+                    <a className="text-gray-600 dark:text-gray-400 hover:underline transition duration-150 ease-in-out" href="https://u-mail.ai" target="_blank">
                       UMail.ai
                     </a>
                   </li>

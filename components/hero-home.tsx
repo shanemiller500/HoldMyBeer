d7926a478e6b0f5e92f404b3655f60998605c254
shanemiller500 <shanemiller500@gmail.com>
2024-10-11 12:43:17 -0600

    text

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -33,7 +33,7 @@ export default function HeroHome() {
                   <svg className="w-3 h-3 fill-current text-teal-400 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                   </svg>
-                  <span>No "Sponsored" Inbox Spam</span>
+                  <span>No "Sponsored" Spam</span>
                 </li>
                 <li className="flex items-center mb-2">
                   <svg className="w-3 h-3 fill-current text-teal-400 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">

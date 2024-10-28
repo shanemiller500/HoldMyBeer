e29a8e0711c0dc3e84d5de28a6a07498bb80aade
shanemiller500 <shanemiller500@gmail.com>
2024-10-28 08:54:02 -0600

    typo

--- components/testimonials-video.tsx
+++ components/testimonials-video.tsx
@@ -1,5 +1,5 @@
 import VideoThumb from '@/public/images/video-testimonial.jpg'
-import ModalVideo from '@/components/modal-video'
+
 
 export default function TestimonialsVideo() {
   return (
@@ -7,14 +7,7 @@ export default function TestimonialsVideo() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pb-12 md:pb-20">
 
-          <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={1024}
-            thumbHeight={576}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080} />
+
 
         </div>
       </div>

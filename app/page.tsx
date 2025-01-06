ef4cdd19559214e8d69a1a87a468edc51954b1ac
shanemiller500 <shanemiller500@gmail.com>
2025-01-06 10:06:37 -0700

    meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,66 @@
 export const metadata = {
-  title: 'Home - HMB-CO',
-  description: 'Page description',
+  title: 'Home - HMB-CO | AI-Powered Solutions for Modern Businesses',
+  description: 'Discover HMB-CO’s cutting-edge AI applications—like ApplyPro & Umail—that drive innovation, streamline workflows, and accelerate business growth. From productivity to communications, our intelligent solutions empower organizations to thrive in the digital age.',
+  keywords: [
+    'HMB-CO',
+    'HMBCO',
+    'Hold My Beer',
+    'Hold My Beer Co',
+    'ApplyPro',
+    'Umail',
+    'AI technology',
+    'AI tools',
+    'AI apps',
+    'Artificial intelligence',
+    'Machine learning',
+    'Generative AI',
+    'Natural language processing',
+    'AI-powered solutions',
+    'Productivity apps',
+    'Workflow automation',
+    'Intelligent automation',
+    'Business innovation',
+    'Digital transformation',
+    'Collaboration tools',
+    'Software solutions',
+    'Enterprise-grade AI',
+    'Smart communication',
+    'Data-driven insights',
+    'Cloud-based AI',
+    'Next-generation technology',
+    'Scalable AI platforms',
+    'Autonomous agents',
+    'Business process automation',
+    'Innovation in AI'
+  ]
+  .join(', '),
+
+  // Open Graph (Facebook, LinkedIn, etc.)
+  openGraph: {
+    type: 'website',
+    url: 'https://holdmybeer.info/',
+    title: 'HMBCO | AI-Powered Solutions for Modern Businesses',
+    description: 'Revolutionize your business with HMB-CO’s suite of AI applications like ApplyPro & Umail. Streamline workflows, empower collaboration, and unlock new growth opportunities using cutting-edge technology.',
+    images: [
+      {
+        url: 'https://holdmybeer.info/images/hmbcoColor.png',
+        width: 1200,
+        height: 630,
+        alt: 'HMB-CO banner',
+      },
+    ],
+    siteName: 'HMB-CO',
+  },
+
+  // // Twitter Card
+  // twitter: {
+  //   card: 'summary_large_image',
+  //   site: '@HMB_CO', // Replace with your Twitter handle
+  //   creator: '@HMB_CO', // Replace with the author's Twitter handle
+  //   title: 'HMB-CO | AI-Powered Solutions for Modern Businesses',
+  //   description: 'Discover HMB-CO’s cutting-edge AI applications—like ApplyPro & Umail—that drive innovation, streamline workflows, and accelerate business growth. From productivity to communications, our intelligent solutions empower organizations to thrive in the digital age.',
+  //   images: ['https://www.hmb-co.com/images/hmb-co-og.jpg'],
+  // },
 }
 
 import PageIllustration from '@/components/page-illustration'
@@ -17,10 +77,7 @@ export default function Home() {
         <PageIllustration />
       </div>
       <Hero />
-    
-
       <Tabs />
-      
       <FeaturesBlocks />
       <Cta />
     </>

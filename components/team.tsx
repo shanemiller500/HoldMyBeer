10863a01f8240694850dc7db5db06c01e0a2f7e9
shanemiller500 <shanemiller500@gmail.com>
2025-02-08 09:20:14 -0700

    added web

--- components/team.tsx
+++ components/team.tsx
@@ -1,10 +1,12 @@
-'use client'; // Must be the first line
+"use client";
 
 import Image from 'next/image';
 import TeamImage01 from '@/public/images/wedding.jpg';
 import { trackEvent } from '@/utils/mixpanel';
-import Link from 'next/link'; // Import Link from next/link
-import { FaLinkedinIn } from 'react-icons/fa6'; // Import LinkedIn icon from React Icons
+import Link from 'next/link';
+import { FaLinkedinIn } from 'react-icons/fa6';
+import { TbWorldWww } from "react-icons/tb";
+
 
 const handleLinkedInClick = (memberName: string) => {
   trackEvent('LinkedIn Clicked', { member: memberName });
@@ -19,7 +21,9 @@ export default function Team() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 font-red-hat-display mb-4">The team</h2>
-            {/* <p className="text-xl text-gray-600 dark:text-gray-400">A one man army ready to take on the world</p> */}
+            {/* <p className="text-xl text-gray-600 dark:text-gray-400">
+              A one man army ready to take on the world
+            </p> */}
           </div>
 
           {/* Team members */}
@@ -27,7 +31,11 @@ export default function Team() {
             <div className="flex flex-wrap justify-center -m-5 lg:-my-7" data-aos-id-team>
 
               {/* 1st member */}
-              <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-5 lg:py-7" data-aos="fade-down" data-aos-anchor="[data-aos-id-team]">
+              <div 
+                className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-5 lg:py-7" 
+                data-aos="fade-down" 
+                data-aos-anchor="[data-aos-id-team]"
+              >
                 <div className="flex flex-col items-center">
                   <Image 
                     className="rounded-full mb-2" 
@@ -36,21 +44,39 @@ export default function Team() {
                     height={80} 
                     alt="Team member 01" 
                   />
-                  <h4 className="dark:text-gray-300 text-lg font-medium">Shane Miller</h4>
-                  <Link 
-                    href="https://www.linkedin.com/in/shane-miller-ninja/"
-                    className="p-2"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="LinkedIn"
-                    onClick={() => handleLinkedInClick('Shane Miller')} // Attach the onClick handler here
-                  >
-                    {/* Using React Icons for LinkedIn */}
-                    <FaLinkedinIn 
-                      size={24} 
-                      className="text-blue-600 hover:text-blue-800 transition-colors duration-200"
-                    />
-                  </Link>
+                  <h4 className="dark:text-gray-300 text-lg font-medium">
+                    Shane Miller
+                  </h4>
+                  <div className="flex space-x-2">
+                    <Link 
+                      href="https://www.linkedin.com/in/shane-miller-ninja/"
+                      className="p-2"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label="LinkedIn"
+                      onClick={() => handleLinkedInClick('Shane Miller')}
+                    >
+                      {/* LinkedIn Icon */}
+                      <FaLinkedinIn 
+                        size={24} 
+                        className="text-indigo-600 hover:text-indigo-800 transition-colors duration-200"
+                      />
+                    </Link>
+                    <Link 
+                      href="https://shanemiller.ninja"
+                      className="p-2"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label="Web site"
+                      onClick={() => handleLinkedInClick('Shane Miller')}
+                    >
+                      {/* Website style image */}
+                      <TbWorldWww 
+                        size={24} 
+                        className="text-indigo-600 hover:text-indigo-800 transition-colors duration-200"
+                      />
+                    </Link>
+                  </div>
                 </div>
               </div>
 
